0fca3d8d4b91ff88e6179254ae06c0cde5e592f8
Daniel Isham <danielisham1@gmail.com>
2017-05-09 18:11:38 -0500

    eval function replaced

--- js/index.js
+++ js/index.js
@@ -6,8 +6,56 @@ $(document).ready(function() {
   let $operator = $('.operator');
   let $equals = $('#equals');
   let expression = [];
+  let num1 = 0;
+  let num2 = 0;
+  let oper = "";
+  let result;
 
-  // tofixed
+  let mathObj = {
+    '+': add,
+    '-': sub,
+    '*': mult,
+    '/': div,
+  };
+
+  function add(a, b) {
+    return a + b;
+  };
+
+  function sub(a, b) {
+    return a - b;
+  };
+
+  function mult(a, b) {
+    return a * b;
+  };
+
+  function div(a, b) {
+    return a / b;
+  };
+
+  function evaluate(arr) {
+    let count = 0;
+    let op = 0;
+    num1 = 0;
+    num2 = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (Number.isInteger(arr[i])) {
+        if (op === 0) {
+          num1 = (num1 * 10) + arr[i];
+          count++;
+        } else {
+          num2 = (num2 * 10) + arr[i];
+          count++;
+        }
+      } else {
+        oper = arr[i];
+        count++;
+        op++;
+      }
+    }
+  }
 
   $span.click(function() {
     $screen.append(($(this).text()));
@@ -15,16 +63,26 @@ $(document).ready(function() {
       expression.push('/');
     } else if (($(this).text()) === 'x') {
       expression.push('*');
+    } else if (($(this).text()) === '+') {
+      expression.push('+');
+    } else if (($(this).text()) === '-') {
+      expression.push('-');
     } else if (($(this).text()) === '=') {
-      let solution = eval(expression.join(''));
-      $screen.text(solution);
+      evaluate(expression);
+      result = mathObj[oper](num1, num2);
+      $screen.text(result);
+      expression = [];
+      expression[0] = result;
     } else if (($(this).text()) === 'C') {
       $screen.text('');
       expression = [];
+      num1 = 0;
+      num2 = 0;
+      oper = "";
     } else {
-      expression.push(($(this).text()));
+      expression.push(parseInt(($(this).text())));
     }
-    console.log(expression);
+
   })
 
 });
